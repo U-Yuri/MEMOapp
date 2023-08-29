ac07a4ac9eb32e5a6b7b6a38bae69d702d9efda5
yuri <80314978+U-Yuri@users.noreply.github.com>
2023-08-29 13:44:03 +0900

    get post patch deleteまでの実装（idはまだ未完成）

---
+++ memo.rb
@@ -0,0 +1,69 @@
+require 'sinatra'
+require 'json'
+require 'debug'
+
+def file
+  File.open("public/memos.json") do |f|
+  	JSON.load(f)
+  end
+end
+
+get '/' do
+    redirect '/memos'
+end
+
+get '/memos' do
+	@memos = file
+	erb :index
+end
+
+get '/memos/new_memo' do
+  erb :new_memo
+end
+
+post '/memos/new_memo/add_new' do
+  memos = file
+	@memo = memos[params[:id]]
+  next_id = memos.max[0].to_i + 1
+  memos.store(next_id, {title: params[:title], comment: params[:comment]})
+  File.open("public/memos.json", "w") do |file|
+    JSON.dump(memos, file)
+  end
+  redirect '/memos'
+end
+
+get '/memos/:id' do
+	memos = file
+	@memo = memos[params[:id]]
+  @id = params[:id]
+  erb :show_memo
+end
+
+get '/memos/:id/edit_memo' do
+  memos = file
+	@memo = memos[params[:id]]
+  @id = params[:id]
+  erb :edit_memo
+end
+
+patch '/memos/:id' do
+  memos = file
+	@memo = memos[params[:id]]
+  id = params[:id]
+  memos.store(999, {title: params[:title], comment: params[:comment]})
+  File.open("public/memos.json", "w") do |file|
+    JSON.dump(memos, file)
+  end
+  p memos
+  redirect "/memos/#{id}"
+end
+
+delete '/memos/:id' do
+  memos = file
+	@memo = memos[params[:id]]
+  memos.delete(params[:id])
+  File.open("public/memos.json", "w") do |file|
+    JSON.dump(memos, file)
+  end
+  redirect '/memos'
+end
